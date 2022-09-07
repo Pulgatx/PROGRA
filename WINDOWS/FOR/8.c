d4ef54bd76f20159a9dc57918fd823b0e0f00c81
Pulgatx <96159199+Pulgatx@users.noreply.github.com>
2022-09-06 22:37:13 -0500

    punto 8

--- WINDOWS/FOR/8.c
+++ WINDOWS/FOR/8.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+void par();
+
 int main()
 {
-    
+    par();
+
+    return 0;    
+}
+
+void par()
+{
+    int j=1;
+    for(int i=0;i<=9;i=i+1)
+    {
+        printf("%i %i\n", i,j);
+
+        if(i!=0)
+            if(i%2)
+                j=j+1;
+    }
+    return;
 }
